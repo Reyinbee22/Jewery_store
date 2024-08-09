9b475671161c64ae446c080b7ad41da142d70e21
Wealth Bassey <163125413+Reyinbee22@users.noreply.github.com>
2024-08-09 21:37:02 +0100

    main

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin:'https://jewery-ediblaire.vercel.app',
-  credentials:true,
-}));
+app.use(cors());
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
